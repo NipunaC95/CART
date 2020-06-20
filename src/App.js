6db2861faf95f21cb81df39de0d88f1ef9ae4e5b
Nipuna <chandimal.nipuna.1995@gmail.com>
2020-06-20 19:43:02 +0530

    added redux base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = ()  => {
+  return (
+    
+      <View>
+      <Text>Hello !</Text>
+      </View>
+  );
+}; 
+
+export default App;
